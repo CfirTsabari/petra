172cb444b9278658a421af3c2d7713093ffe6dbb
CfirTsabari <cfir16@gmail.com>
2023-07-30 18:59:25 +0000

    chore: feature for each impl

--- crates/petra-backend/src/config.rs
+++ crates/petra-backend/src/config.rs
@@ -1,9 +1,13 @@
 pub use petra_backend_core::BackendConfiguration;
+#[cfg(feature = "csharp")]
 pub use petra_backend_csharp::PetraCSharpConfiguration;
+#[cfg(feature = "golang")]
 pub use petra_backend_golang::PetraGolangConfiguration;
 #[derive(Default)]
 pub struct PetraConfiguration {
+    #[cfg(feature = "golang")]
     golang: Option<PetraGolangConfiguration>,
+    #[cfg(feature = "csharp")]
     csharp: Option<PetraCSharpConfiguration>,
 }
 impl PetraConfiguration {
@@ -11,19 +15,25 @@ impl PetraConfiguration {
     pub fn new() -> Self {
         Self::default()
     }
+}
+#[cfg(feature = "golang")]
+impl PetraConfiguration {
     pub fn set_golang(&mut self, golang: PetraGolangConfiguration) {
         self.golang = Some(golang);
-    }
-    pub fn set_csharp(&mut self, csharp: PetraCSharpConfiguration) {
-        self.csharp = Some(csharp);
     }
 
     #[must_use]
     pub fn golang(self) -> PetraGolangConfiguration {
         self.golang.unwrap_or_default()
     }
+}
+#[cfg(feature = "csharp")]
+impl PetraConfiguration {
     #[must_use]
     pub fn csharp(self) -> PetraCSharpConfiguration {
         self.csharp.unwrap_or_default()
+    }
+    pub fn set_csharp(&mut self, csharp: PetraCSharpConfiguration) {
+        self.csharp = Some(csharp);
     }
 }
--- crates/petra/src/backend_type.rs
+++ crates/petra/src/backend_type.rs
@@ -4,27 +4,35 @@ use petra_backend::BackendType as CoreBackendType;
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 #[value(rename_all = "PascalCase")]
 pub enum BackendType {
+    #[cfg(feature = "python")]
     #[value(aliases(["python","py"]))]
     Python,
+    #[cfg(feature = "golang")]
     #[value(aliases(["golang","go"]))]
     GoLang,
     #[value(aliases(["typescript","ts"]))]
     TypeScript,
     #[value(aliases(["javascript","js"]))]
     JavaScript,
+    #[cfg(feature = "rust")]
     #[value(aliases(["rust","rs"]))]
     Rust,
+    #[cfg(feature = "csharp")]
     #[value(aliases(["csharp","C#","c#"]))]
     CSharp,
 }
 impl From<BackendType> for CoreBackendType {
     fn from(val: BackendType) -> Self {
         match val {
+            #[cfg(feature = "python")]
             BackendType::Python => Self::Python,
+            #[cfg(feature = "golang")]
             BackendType::GoLang => Self::GoLang,
             BackendType::TypeScript => Self::TypeScript,
             BackendType::JavaScript => Self::JavaScript,
+            #[cfg(feature = "rust")]
             BackendType::Rust => Self::Rust,
+            #[cfg(feature = "csharp")]
             BackendType::CSharp => Self::CSharp,
         }
     }
